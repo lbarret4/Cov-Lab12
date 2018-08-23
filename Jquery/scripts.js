5121ef4f03f42246f5b2b503145b66a3133a720b
lbarret4 <llewellynbar4@gmail.com>
2018-08-23 14:13:21 -0400

    Change button text and have button node places before script tag

--- Jquery/scripts.js
+++ Jquery/scripts.js
@@ -1,10 +1,11 @@
 //1. Add Button to jQuery page with an alert('') action when pressed
-let button = $('<button type=\'submit\'>Submit</button>').appendTo('body');
-button.click(function(e){
-  e.preventDefault();
-  alert('Greetings!')
-  
-});
+let button = $('<button type=\'submit\'>Greeting</button>').insertBefore('script[src*=\'query\']');
+  button.click(function(e){
+    e.preventDefault();
+    alert('Greetings!')
+    
+    
+  });
 
 //2. Adds an event listener for a clicked textbox button, where the text box content is displayed in an alert message.
 
